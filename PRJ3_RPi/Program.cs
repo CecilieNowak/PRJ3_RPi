e6e6bd83e4bf7bef976c83b7acc34bb60af0eac1
Cecilie Svangtun Nowak <201910286@post.au.dk>
2021-11-03 09:12:35 +0100

    Add project files.

---
+++ PRJ3_RPi/Program.cs
@@ -0,0 +1,17 @@
+﻿using RaspberryPiCore.ADC;
+using RaspberryPiCore.JoySticks;
+using RaspberryPiCore.LCD;
+using RaspberryPiCore.TWIST;
+using System;
+
+
+namespace Raspberry_Pi_Dot_Net_Core_Console_Application3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
