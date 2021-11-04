3defd57ce9dc0f6a6dc52ccd13c1f2d6fca8e138
Abdul1997 <201711212@post.au.dk>
2021-11-04 13:47:22 +0100

    test2

--- DataLayer/BPData.cs
+++ DataLayer/BPData.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Text;
 using RaspberryPiCore.ADC;
 using DTO;
+
 namespace DataLayer
 {
     public class BPData
     {
         private ADC1015 aDC;
+       
 
         public BPData(ADC1015 adc)
         {
@@ -20,16 +22,17 @@ namespace DataLayer
             List<DTO_BPressure> blodtryk = new List<DTO_BPressure> { }; 
             DTO_BPressure BP= new DTO_BPressure(0);
 
-
-
-            BP = new DTO_BPressure(aDC.readADC_Differential_0_1());
-
-            blodtryk.Add(BP);
-
-
-
-
+            while (true) // (så længe vi får værdier fra ADC) 
+            {
+                BP = new DTO_BPressure(aDC.readADC_Differential_0_1());
+                blodtryk.Add(BP);
+            }
+            
+          
             return blodtryk;
+
+
+
         }
     }
 }
