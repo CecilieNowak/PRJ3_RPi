543e1684f46a5cc95c834742ad294f2f606b5de6
Abdul1997 <201711212@post.au.dk>
2021-11-25 13:05:02 +0100

    25/11

--- DataLag/UDPSender.cs
+++ DataLag/UDPSender.cs
@@ -1,29 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
 using DTO;
 using System.Text.Json;
 using System.Collections.Concurrent;
-using System.Net;
-using System.Net.Sockets;
 
 namespace DataLag
 {
     public class UDPSender
     {
-
         private const int listenPortCommand = 12000;
-        private BPData1 blod;
+        private BPData blod;
+
+        //        private List<DTO_BPressure> blodtryklist;
         private readonly BlockingCollection<DTO_BPressure> _dataQueue;
         private byte[] jsonUtf8Bytes;
-
 
         public UDPSender(BlockingCollection<DTO_BPressure> dataQueue)
         {
             _dataQueue = dataQueue;
+            
         }
+
 
 
         public void SendData()
@@ -32,13 +32,14 @@ namespace DataLag
             IPAddress broadcast = IPAddress.Parse("192.168.1.117");                                              // opsætning af UDPSender protokol
             IPEndPoint ep = new IPEndPoint(broadcast, 12000);
 
+
             while (!_dataQueue.IsCompleted)
             {
                 try
                 {
 
                     var container = _dataQueue.Take();
-
+                    
 
                     JsonSerializerOptions sendValue = new JsonSerializerOptions() { WriteIndented = true }; // De her 3 linjer har den opgave at oversætte vores blodData objekt til Byte, således at SendTo funktion kan forstår den 
                     jsonUtf8Bytes = JsonSerializer.SerializeToUtf8Bytes(container, sendValue);
@@ -49,12 +50,18 @@ namespace DataLag
                 catch (InvalidOperationException)
                 {
 
+
                 }
 
             }
 
 
-        }
 
+
+
+
+
+        }
     }
 }
+
---
+++ RPi_dotnet_core_Console_Application1/Program.cs
@@ -0,0 +1,36 @@
+﻿using DTO;
+using RaspberryPiNetCore.ADC;
+using RaspberryPiNetCore.JoySticks;
+using RaspberryPiNetCore.LCD;
+using RaspberryPiNetCore.TWIST;
+using System;
+using System.Collections.Concurrent;
+using System.Threading;
+using DataLag;
+
+namespace PresentationLayer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (!Console.KeyAvailable)
+            {
+                BlockingCollection<DTO_BPressure> dataQueue = new BlockingCollection<DTO_BPressure>();
+
+
+                BPData bpData = new BPData(dataQueue);
+                UDPSender udpSender = new UDPSender(dataQueue);
+
+                Thread producerThread = new Thread(bpData.Run);
+                Thread consumerThread = new Thread(udpSender.SendData);
+
+                producerThread.Start();
+                consumerThread.Start();
+
+                Console.ReadKey();
+
+            }
+        }
+    }
+}
